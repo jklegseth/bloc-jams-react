06f8f00824002d7fcee81c2962d7545f6b1639b4
Junior Klegseth <jklegseth@gmail.com>
2017-10-23 16:15:35 -0700

    added landing page basic structure and content

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
 const Landing = () => (
-  <section className="landing">
-    Landing page will go here
-  </section>
+  <div>
+    <h1 class="hero-title">Turn the music up!</h1>
+
+    <section class="selling-points">
+      <div class="point">
+        <h2 class="point-title">Choose your music</h2>
+        <p class="point-description">The world is full of music; why should you have to listen to music that someone else chose?</p>
+      </div>
+      <div class="point">
+        <h2 class="point-title">Unlimited, streaming, ad-free</h2>
+        <p class="point-description">No arbitrary limits. No distractions.</p>
+      </div>
+      <div class="point">
+        <h2 class="point-title">Mobile enabled</h2>
+        <p class="point-description">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
+      </div>
+    </section>
+  </div>
 );
 
 export default Landing;
