290a5ab5aff05f9d4a6588df959b7fc87dd645be
Junior Klegseth <jklegseth@gmail.com>
2017-10-26 13:32:46 -0700

    events, changed to use SongRow component, new state and methods

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
 import PlayerBar from './PlayerBar';
+import SongRow from './SongRow';
 
 
 class Album extends Component {
@@ -14,29 +15,67 @@ class Album extends Component {
     this.state = {
       album: album,
       currentSong: album.songs[0],
+      currentSongIndex: 0,
       currentTime: 0,
       currentVolume: .7,
       duration: album.songs[0].duration,
-      isPlaying: false
+      isPlaying: false,
+      isPaused: false,
+      isMuted: false,
+      premuteVolume: .7
     }
 
     this.audioElement = document.createElement('audio');
     this.audioElement.src = album.songs[0].audioSrc;
+
+    // need to do this here to be able to use `removeEventListener`
+    this.onTimeupdate = this.onTimeupdate.bind(this);
+    this.onDurationChange = this.onDurationChange.bind(this);
+    this.onVolumeChange = this.onVolumeChange.bind(this);
+    this.onEnded = this.onEnded.bind(this);
   }
 
   play(song) {
     this.audioElement.play();
-    this.setState({ isPlaying: true });
+    this.setState({
+      isPlaying: true,
+      isPaused: false
+    });
   }
 
   pause(song) {
     this.audioElement.pause();
-    this.setState({ isPlaying: false });
+    this.setState({
+      isPlaying: false,
+      isPaused: true
+    });
+  }
+
+  toggleMute() {
+    console.log(this.state.isMuted);
+    if (this.state.isMuted) {
+      this.setState({
+        currentVolume: this.state.premuteVolume,
+        isMuted: false
+      });
+      this.audioElement.volume = this.state.premuteVolume;
+    } else {
+      this.setState({
+        premuteVolume: this.state.currentVolume,
+        currentVolume: 0,
+        isMuted: true
+      });
+      this.audioElement.volume = 0;
+    }
   }
 
   setSong(song) {
     this.audioElement.src = song.audioSrc;
-    this.setState({ currentSong: song });
+    const currentIndex = this.state.album.songs.findIndex(item => song === item);
+    this.setState({
+      currentSong: song,
+      currentSongIndex: currentIndex
+    });
   }
 
   handleSongClick(song) {
@@ -88,66 +127,91 @@ class Album extends Component {
     return `${minutes}:${seconds}`;
   }
 
+  onTimeupdate() {
+    this.setState({ currentTime: this.audioElement.currentTime });
+  }
+
+  onDurationChange() {
+    this.setState({ duration: this.audioElement.duration });
+  }
+
+  onVolumeChange() {
+    this.setState({ currentVolume: this.audioElement.volume });
+  }
+
+  onEnded() {
+    this.handleNextClick();
+  }
+
   componentDidMount() {
-    this.audioElement.addEventListener('timeupdate', e => {
-      this.setState({ currentTime: this.audioElement.currentTime })
-    });
+    this.audioElement.addEventListener('timeupdate', this.onTimeupdate);
+    this.audioElement.addEventListener('durationchange', this.onDurationChange);
+    this.audioElement.addEventListener('volumechange', this.onVolumeChange);
+    this.audioElement.addEventListener('ended', this.onEnded);
+  }
 
-    this.audioElement.addEventListener('durationchange', e => {
-      this.setState({ duration: this.audioElement.duration })
-    });
-
-    this.audioElement.addEventListener('volumechange', e => {
-      this.setState({ currentVolume: this.audioElement.volume })
-    });
+  componentWillUnmount() {
+    this.audioElement.removeEventListener('timeupdate', this.onTimeupdate);
+    this.audioElement.removeEventListener('durationchange', this.onDurationChange);
+    this.audioElement.removeEventListener('volumechange', this.onVolumeChange);
+    this.audioElement.removeEventListener('ended', this.onEnded);
+    this.pause();
   }
 
   render() {
     return (
-      <div>
-        <section id="album-info">
-          <img id="album-cover-art" src={ this.state.album.albumCover } alt={this.state.album.title} />
-          <div className="album-details">
-            <h1 id="album-title">{ this.state.album.title }</h1>
-            <h2 className="artist">{ this.state.album.artist }</h2>
-            <div id="release-info">{ this.state.album.releaseInfo }</div>
+      <div className="album">
+        <section className="container narrow">
+          <div className="clearfix">
+            <div className="column half">
+              <img src={ this.state.album.albumCover } className="album-cover" alt={this.state.album.title} />
+            </div>
+            <div className="album-details column half">
+              <h2 className="album-title" id="album-title">{ this.state.album.title }</h2>
+              <h3 className="album-artist">{ this.state.album.artist }</h3>
+              <h5 className="album-release-info" id="release-info">{ this.state.album.releaseInfo }</h5>
+            </div>
           </div>
         </section>
-        <table id="song-list">
-          <colgroup>
-            <col id="song-number-column" />
-            <col id="song-title-column" />
-            <col id="song-duration-column" />
-          </colgroup>
-          <tbody>
-          {
-            this.state.album.songs.map((song, index) =>
-              <tr className="song" key={ index } onClick={() => this.handleSongClick(song)}>
-                <td className="song-actions">
-                  <span className="song-number">{index+1}</span>
-                  <span className="ion-play"></span>
-                  <span className="ion-pause"></span>
-                </td>
-                <td className="song-item-title">{ song.title }</td>
-                <td className="song-item-duration">{ this.formatTime(song.duration) }</td>
-              </tr>
-            )
-          }
-          </tbody>
-        </table>
-        <PlayerBar
-          isPlaying={ this.state.isPlaying }
-          currentSong={ this.state.currentSong }
-          currentTime={ this.audioElement.currentTime }
-          currentVolume={ this.state.currentVolume }
-          duration={ this.audioElement.duration }
-          handleSongClick={ () => this.handleSongClick(this.state.currentSong) }
-          handlePrevClick={ () => this.handlePrevClick() }
-          handleNextClick={ () => this.handleNextClick() }
-          handleTimeChange={ (e) => this.handleTimeChange(e) }
-          handleVolumeChange={ (e) => this.handleVolumeChange(e) }
-          formatTime={ (seconds) => this.formatTime(seconds) }
-        />
+        <div className="album-song-list-container">
+          <table className="album-song-list">
+            <colgroup>
+              <col id="song-number-column" />
+              <col id="song-title-column" />
+              <col id="song-duration-column" />
+            </colgroup>
+            <tbody>
+            {
+              this.state.album.songs.map((song, index) => {
+                return <SongRow
+                  song={ song }
+                  key={ index }
+                  index={ index }
+                  isPlaying={ this.state.isPlaying }
+                  isPaused={ this.state.isPaused }
+                  isSelectedSong={ index === this.state.currentSongIndex }
+                  handleSongClick={ () => this.handleSongClick(song) }
+                />
+              })
+            }
+            </tbody>
+          </table>
+          <PlayerBar
+            isPlaying={ this.state.isPlaying }
+            currentSong={ this.state.currentSong }
+            currentTime={ this.audioElement.currentTime }
+            currentVolume={ this.state.currentVolume }
+            duration={ this.audioElement.duration }
+            isMuted={ this.state.isMuted }
+            handleSongClick={ () => this.handleSongClick(this.state.currentSong) }
+            handlePrevClick={ () => this.handlePrevClick() }
+            handleNextClick={ () => this.handleNextClick() }
+            handleTimeChange={ (e) => this.handleTimeChange(e) }
+            handleVolumeChange={ (e) => this.handleVolumeChange(e) }
+            formatTime={ (seconds) => this.formatTime(seconds) }
+            toggleMute={ () => this.toggleMute() }
+          />
+        </div>
       </div>
     );
   }
