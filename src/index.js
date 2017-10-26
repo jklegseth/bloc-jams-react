f004d0466796cfe54943ac90a58f5417d099b747
Junior Klegseth <jklegseth@gmail.com>
2017-10-26 13:28:28 -0700

    added css imports

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import './css/normalize.css';
+import './css/global.css';
+import './css/album.css';
+import './css/song-row.css';
+import './css/library.css';
+import './css/landing.css';
+import './css/player_bar.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
